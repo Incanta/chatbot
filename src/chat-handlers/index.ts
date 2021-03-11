985cb2f5ff52adc641d562d30e99f0b929c0b92c
Mike Seese <seesemichaelj@gmail.com>
2021-03-10 23:03:54 -0800

    add the shout out handler

--- src/chat-handlers/index.ts
+++ src/chat-handlers/index.ts
@@ -1,6 +1,7 @@
 import config from "config";
 
 import { ChatHandler } from "./chat-handler";
+import { ShoutOutHandler } from "./shout-out";
 import { SimpleHandler } from "./simple";
 
 export interface IChatHandlerConfig {
@@ -18,7 +19,7 @@ export async function InitializeHandlers(): Promise<ChatHandler[]> {
   const handlers: ChatHandler[] = [];
 
   const preconfiguredChatHandlers = config.get<IChatHandlerListItems>("chat-handlers");
-  for (const [_hanlderName, handlerConfig] of Object.entries(preconfiguredChatHandlers)) {
+  for (const [handlerName, handlerConfig] of Object.entries(preconfiguredChatHandlers)) {
     if (handlerConfig.enabled) {
       if (handlerConfig.response !== null) {
         const handler = new SimpleHandler(handlerConfig.command, handlerConfig.response, handlerConfig.aliases);
@@ -26,6 +27,14 @@ export async function InitializeHandlers(): Promise<ChatHandler[]> {
       } else {
         // here is where custom chat handlers get initialized
         // these would be anything that isn't a simple, fixed response
+        if (handlerName === "so") {
+          const handler = new ShoutOutHandler(
+            handlerConfig.command,
+            config.get<string>("shout-outs.generic-message"),
+            handlerConfig.aliases
+          );
+          handlers.push(handler);
+        }
       }
     }
   }
---
+++ src/chat-handlers/shout-out.ts
@@ -0,0 +1,33 @@
+import config from "config";
+
+import { ChatHandler } from "./chat-handler";
+
+export class ShoutOutHandler implements ChatHandler {
+  private response: string;
+  private aliases: string[];
+
+  constructor(command: string, response: string, aliases: string[] | null) {
+    this.response = response;
+    this.aliases = (aliases || []).concat(command);
+  }
+
+  public async handle(
+    _channel: string,
+    message: string,
+    _user: string
+  ): Promise<string | null> {
+    const trimmedMessage = message.trim();
+    const regex = new RegExp(`^(${this.aliases.join("|").replace(/!/g, "\\!")}) (.+)`);
+    const match = regex.exec(trimmedMessage);
+    if (match && match.length >= 3) {
+      const username = match[2].toLowerCase();
+      const message = config.has(`shout-outs.list.${username}`) ?
+        config.get<string>(`shout-outs.list.${username}.message`) :
+        this.response.replace(/_USER_/g, username)
+
+      return message;
+    } else {
+      return null;
+    }
+  }
+}
